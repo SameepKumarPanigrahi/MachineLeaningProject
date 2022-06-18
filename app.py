645d91564b8ca6dfb171484dc7c5cd56379dfc84
sameepkumarpanigrahi <sameepkumarpanigrahi@gmail.com>
2022-06-18 10:38:45 +0530

    commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    return ("CI CD Pipeline is established now the real work begins")
+    return ("CI CD Pipeline is established now the real work begins : ")
     
 if __name__ == '__main__':
     app.run(debug=True)
