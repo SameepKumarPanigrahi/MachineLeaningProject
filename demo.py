972404d1e1bb2064fcdbd3fdf03171c7e87a879c
sameepkumarpanigrahi <sameepkumarpanigrahi@gmail.com>
2022-07-09 20:06:55 +0530

    UPdated

---
+++ demo.py
@@ -0,0 +1,15 @@
+from housing.pipeline.pipeline import Pipeline
+from housing.exception import HousingException
+from housing.logger import logging
+from housing.config.configuration import Configuration
+def main():
+    try:
+        pipeline = Pipeline()
+        pipeline.run_pipeline()
+        # data_transformation_config = Configuration().get_data_transformation_config()
+        # print(data_transformation_config)
+    except Exception as e:
+        logging.error(f"{e}")
+        print(e)
+if __name__=="__main__":
+    main()
---
+++ housing/entity/model_factory.py
@@ -0,0 +1,37 @@
+from housing.logger import logging
+from housing.exception import HousingException
+from collections import namedtuple
+import sys, os
+
+
+GRID_SEARCH_KEY = 'grid_search'
+MODULE_KEY = 'module'
+CLASS_KEY = 'class'
+PARAM_KEY = 'params'
+MODEL_SELECTION_KEY = 'model_selection'
+SEARCH_PARAM_GRID_KEY = "search_param_grid"
+
+InitializedModelDetail = namedtuple("InitializedModelDetail",
+                                    ["model_serial_number", "model", "param_grid_search", "model_name"])
+
+GridSearchedBestModel = namedtuple("GridSearchedBestModel", ["model_serial_number",
+                                                             "model",
+                                                             "best_model",
+                                                             "best_parameters",
+                                                             "best_score",
+                                                             ])
+
+BestModel = namedtuple("BestModel", ["model_serial_number",
+                                     "model",
+                                     "best_model",
+                                     "best_parameters",
+                                     "best_score", ])
+
+MetricInfoArtifact = namedtuple("MetricInfoArtifact",
+                                ["model_name", "model_object", "train_rmse", "test_rmse", "train_accuracy",
+                                 "test_accuracy", "model_accuracy", "index_number"])
+
+
+class ModelFactory:
+    def __init__(self):
+        pass
